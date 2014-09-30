e054ba76aa4ef29692a420d7553914aca8216768
joshfarrant <joshuafefarrant@gmail.com>
2014-09-30 17:40:20 +0100

    Design tweaks

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-OAuth.initialize()
+OAuth.initialize();
 
 $('#view-tweets').hide();
 
 $('#btn-sign-in').click(function(){
 
-  // data = {
-  //   oauth_callback : 'https://www.twitter.com/'
-  // }
+  data = {
+    oauth_callback : 'https://www.twitter.com/'
+  };
 
-  // console.log(data)
+  console.log(data);
 
-  // $.ajax({
-  //   type: 'POST',
-  //   url: 'https://api.twitter.com/oauth/request_token',
-  //   data: data,
-  //   success: function(response) {
-  //     console.log(response)
-  //   },
-  //   error: function(response) {
-  //     console.log(response)
-  //   },
-  //   dataType: 'json'
-  // });
+  $.ajax({
+    type: 'POST',
+    url: 'https://api.twitter.com/oauth/request_token',
+    data: data,
+    success: function(response) {
+      console.log(response);
+    },
+    error: function(response) {
+      console.log(response);
+    },
+    dataType: 'jsonp'
+  });
 
-  $('#view-sign-in').hide();
-  $('#view-tweets').show();
+  // $('#view-sign-in').hide();
+  // $('#view-tweets').show();
 
-})
+});
